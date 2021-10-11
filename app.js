db4cedc54978cc119b788aad2dc584670e417123
AndAlloy <kormilo121@gmail.com>
2021-10-11 18:22:37 +0300

    database config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const { db: db } = require('./settings/settigns');
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.set('view engine', 'ejs');
 
 const PORT = 3000;
 
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
+
 app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`);
-  });
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
+app.use(express.urlencoded({ extended: false }));
