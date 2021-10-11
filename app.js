87feca7e7f0e402ca579beeee806c0eb3f7443b4
AndAlloy <kormilo121@gmail.com>
2021-10-11 18:47:10 +0300

    added home page + partials

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const findPath = require('./helpers/find-path');
 const { db: db } = require('./settings/settigns');
 
 const app = express();
@@ -18,4 +19,7 @@ app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.get('/', (req, res) => {
+  const title = 'Home';
+  res.render(findPath('index'), { title });
+});
